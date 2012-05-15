36926eada77ea9c5a8e2fc524d0b4fd8a5acd2f8
Oler Rudchenko <oleg.rudchenko@gmail.com>
2012-05-16 01:37:44 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniTwitter::Application.config.secret_token = 'b1718b3f15cff4ba08c856b6a8521f8a2578798e4d62253cfbba11a097f43d56982c745a343029e457358e4bf369116dbe906a244ede2dd1db9afbd6e9990e66'
